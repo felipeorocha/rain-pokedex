196a6f326a6052a13b35054d1edc91f239934902
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-08-10 01:41:17 -0300

    Project setup basics: API calls, pagination and state planning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
+import { fetchAllPokemons } from './services/api';
 
-function App() {
+const App = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [nextPokemonPage, setNextPokemonPage] = useState('');
+  const [prevPokemonPage, setPrevPokemonPage] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'pokemon';
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await fetchAllPokemons(initialUrl);
+      setNextPokemonPage(res.next);
+      setPrevPokemonPage(res.previous);
+      setLoading(false);
+    }
+
+    getPokemon();
+  }, []);
+
   return (
     <div className="App">
+      {loading ? <h1>Loading...</h1> : (
+        <h1>Good to go!</h1>
+      )}
+
     </div>
   );
 }
---
+++ src/services/api/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pokeapi.co/api/v2/';
+
+const fetchApi = axios.create({
+  baseURL: BASE_URL
+});
+
+export const fetchAllPokemons = url => {
+  return fetchApi.get(url);
+}
+
+export default fetchApi;
