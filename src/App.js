c840b0d420bab079f1531609060e49855cee788d
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-08-11 02:51:02 -0300

    App config: importing and integrationg components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { useEffect, useState } from 'react';
 import {
   fetchPokemons as fetchAllPokemons,
-  fetchPokemons as fetchPokemon
+  fetchPokemons as fetchPokemon,
+  fetchPokemons as searchPokemon
 } from './services/api';
+import Button from './components/Button';
+import Card from './components/Card';
+import Header from './components/Header';
+import Loader from './components/Loader';
+import Search from './components/Search';
+
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
+
+import './App.css';
 
 const App = () => {
   const [pokemon, setPokemon] = useState([]);
   const [nextPokemonPage, setNextPokemonPage] = useState('');
   const [prevPokemonPage, setPrevPokemonPage] = useState('');
   const [loading, setLoading] = useState(true);
+  const [pokemonFilter, setPokemonFilter] = useState('');
   const initialUrl = 'pokemon';
 
   useEffect(() => {
@@ -30,21 +41,72 @@ const App = () => {
     getPokemon();
   }, []);
 
+  const nextPage = async () => {
+    setLoading(true);
+
+    const {
+      data: {
+        next,
+        previous,
+        results
+      }
+    } = await fetchAllPokemons(nextPokemonPage);
+
+    await loadingPokemon(results);
+    setNextPokemonPage(next);
+    setPrevPokemonPage(previous);
+    setLoading(false);
+  }
+
+  const prevPage = async () => {
+    if (!prevPokemonPage) return;
+
+    setLoading(true);
+    const {
+      data: {
+        next,
+        previous,
+        results
+      }
+    } = await fetchAllPokemons(prevPokemonPage);
+
+    await loadingPokemon(results);
+    setNextPokemonPage(next);
+    setPrevPokemonPage(previous);
+    setLoading(false);
+  }
+
   const loadingPokemon = async pokemonList => {
     const pokemons = await Promise.all(
       pokemonList.map(async pokemonItem => {
         const pokemonsListData = await fetchPokemon(pokemonItem.url);
-        return pokemonsListData;
+        return pokemonsListData.data;
     }));
 
     setPokemon(pokemons);
   }
 
-  console.log('poke', pokemon);
   return (
-    <div className="App">
-      {loading ? <h1>Loading...</h1> : (
-        <h1>Good to go!</h1>
+    <div>
+      { loading ? <Loader /> : (
+        <>
+          <Header />
+          <Search filteredPokemon={(query) => setPokemonFilter(query)} />
+          <div className="grid-container">
+            { pokemon && pokemon.map((poke, index) => {
+                return poke.name.includes(pokemonFilter) &&
+                <Card key={index} pokemon={poke} />        
+            }) }
+          </div>
+          <div className="buttons-container">
+            <Button onClick={prevPage}>
+              <FaAngleLeft />
+            </Button>
+            <Button onClick={nextPage}>
+              <FaAngleRight />
+            </Button>
+          </div>
+        </>
       )}
 
     </div>
