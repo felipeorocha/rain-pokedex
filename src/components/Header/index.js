5ffa6610dfa8e36181f652135d78a8543da9877d
Felipe Rocha <felipeo.rocha@hotmail.com>
2021-08-13 14:43:17 -0300

    Headere component refactor

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,28 @@
-import { Header as PageHeader, LogoContainer } from './styles';
+import { Header as PageHeader, LogoContainer, StaredContainer, Star } from './styles';
 import Button from '../Button';
 
 import rain_logo from '../../assets/rain_logo.jpg';
 import auth from '../../services/firebase';
 
-const Header = () => {
+import { FaHeart } from "react-icons/fa";
+
+const Header = ({ history }) => {
+  const redirToStared = () => {
+    history.push('/stared');
+  }
+
   return (
     <PageHeader>
       <LogoContainer>
         <img src={rain_logo} alt="Rain" />
         Rain Instant Pay Pokedex
       </LogoContainer>
-      <div>
+      <StaredContainer>
+        <Star onClick={redirToStared}>
+          <FaHeart />
+        </Star>
         <Button primary onClick={() => auth.auth().signOut()}>Sign Out</Button>
-      </div>
+      </StaredContainer>
 
     </PageHeader>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -19,3 +19,25 @@ export const LogoContainer = styled.div`
   display: flex;
   align-items: center;
 `;
+
+export const StaredContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 180px;
+`;
+
+export const Star = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  cursor: pointer;
+  border-radius: 50%;
+  box-shadow: rgb(0 0 0 / 35%) 0px 5px 15px;
+
+  &:hover {
+    box-shadow: rgb(0 0 0 / 60%) 0px 5px 20px;
+    transition: 0.2s;
+  }
+`;
